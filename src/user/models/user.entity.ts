acdef4a769e93c711bb332dfe123b3b862a87e3d
nesrine benabderrahim <nesrine.abd.softavera@gmail.com>
2025-06-18 22:41:19 +0100

    feat: add `user entity`

---
+++ src/user/models/user.entity.ts
@@ -0,0 +1,37 @@
+import { timeStamp } from "console";
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('user')
+export class userEntity{
+    @PrimaryGeneratedColumn()
+    id:number
+
+    @Column({default:''})
+    nom:string;
+
+    @Column({default:''})
+    prénom:string;
+
+    @Column({default:'client'})
+    role:string;
+
+    @Column({default:''})
+    email:string;
+
+    @Column({ type: 'jsonb',default:null }) // Change the data type to 'jsonb'
+    adresse: any;
+
+    @Column({default: null})
+    shop:number
+
+    @Column({default:0})
+    télephone:number;
+
+    @Column({default:''})
+    password:string;
+
+    @Column({type:'timestamp',default:()=>'CURRENT_TIMESTAMP'})
+    createdAt:Date;
+
+
+}
