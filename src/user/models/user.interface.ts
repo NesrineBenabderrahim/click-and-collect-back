1ca9b37936811ea11e2d801625daf9e3748271e0
nesrine benabderrahim <nesrine.abd.softavera@gmail.com>
2025-06-18 22:41:03 +0100

    feat: add `user interface`

---
+++ src/user/models/user.interface.ts
@@ -0,0 +1,12 @@
+export interface user{
+    id?:number;
+    nom?:string;
+    prénom?:string;
+    email?:string;
+    role?:string;
+    shop?:number;
+    adresse?: any;
+    télephone?:number;
+    password?:string;
+    createdAt?:Date;
+}
